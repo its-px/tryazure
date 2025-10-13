109bf40534f0c576a1a4893e1eecc4f34668ff15
Christos <your.email@example.com>
2025-10-13 11:02:32 +0300

    fixed double node_modules and enable pwa

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,9 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "RENDEZVOUS ME",
+        short_name: "RENDEZVOUS",
+        description: "My awesome PWA built with Vite + React",
+        theme_color: "#1b5e20",
+        background_color: "#3a3a3a",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo5.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/logo5.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     dedupe: ["react", "react-dom", "@emotion/react", "@emotion/styled"],
   },
